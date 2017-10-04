84897182542d058b6eb17b7dc2b28c14b59dc569
René Milk <rene.milk@wwu.de>
2017-10-04 14:49:32 +0200

    [hooks] adds multihook infrastructure
    you can now chain multiple hook-scripts per
    hook name
    see hooks/multihooks.py docstring for detailed instructions

--- run.d/01_install_hooks.bash
+++ run.d/01_install_hooks.bash
@@ -6,4 +6,4 @@ SRC_HOOKDIR="$(cd "$(dirname ${BASH_SOURCE[0]})/../hooks" ;  pwd -P )"
 # this _might_ fail with git >= 2.9 if core.hooksPath is set
 DST_HOOKDIR="$(git rev-parse --git-dir)/hooks"
 
-cp -a ${SRC_HOOKDIR}/* ${DST_HOOKDIR}
+cp -ra ${SRC_HOOKDIR}/* ${DST_HOOKDIR}
