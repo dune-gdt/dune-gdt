549ad4574be7236cdebea008c2caddc725c1a99e
René Fritze <rene.fritze@wwu.de>
2019-03-20 09:54:10 +0100

    env file has to be recreated in steps with inserted secrets

--- make_env_file.py
+++ make_env_file.py
@@ -18,7 +18,7 @@ home = expanduser("~")
 
 prefixes = os.environ.get('ENV_PREFIXES', 'TRAVIS DRONE GITLAB CODECOV CI encrypt TOKEN TESTS').split(' ')
 blacklist = ['TRAVIS_COMMIT_MESSAGE']
-env_file = os.environ.get('ENV_FILE', os.path.join(home, 'env'))
+env_file = os.environ.get('DOCKER_ENVFILE', os.path.join(home, 'env'))
 with open(env_file, 'wt') as env:
     for k, v in os.environ.items():
         for pref in prefixes:
