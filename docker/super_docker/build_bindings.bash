7e430ecfe410fede5a91a82e28bcffe70eab551c
Tobias Leibner <tobias.leibner@googlemail.com>
2019-10-14 15:08:32 +0200

    [docker.super-docker.build_bindings] dune-xt-* -> dune-xt*

--- docker/super_docker/build_bindings.bash
+++ docker/super_docker/build_bindings.bash
@@ -1,5 +1,5 @@
 #!/bin/bash
-for i in $(ls -d dune-xt-* dune-gdt 2> /dev/null ); do
+for i in $(ls -d dune-xt* dune-gdt 2> /dev/null ); do
   cd ${SUPERDIR}/$i
   ${SRC_DCTRL} ${BLD} --only=${i} bexec ${BUILD_CMD} bindings || echo 'no bindings target'
 done
