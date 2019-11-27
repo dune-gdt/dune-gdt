fb2c0668d7c85faaa868ebecdcbf53f0d3743e20
Tobias Leibner <tobias.leibner@googlemail.com>
2019-11-27 08:48:29 +0100

    [test_cpp] try to fix test for empty submodule

--- scripts/test_cpp.bash
+++ scripts/test_cpp.bash
@@ -23,7 +23,7 @@ CTEST="ctest -V --timeout ${DXT_TEST_TIMEOUT:-300} -j ${DXT_TEST_PROCS:-2}"
 rm -rf ${DUNE_BUILD_DIR}
 ${SRC_DCTRL} ${BLD} --module=${MY_MODULE} all
 ${SRC_DCTRL} ${BLD} --only=${MY_MODULE} bexec ${BUILD_CMD}
-if [ -z "${TESTS_MODULE_SUBDIR}" ] ; then
+if [ "${TESTS_MODULE_SUBDIR}" = "None" ] ; then
   ${SRC_DCTRL} ${BLD} --only=${MY_MODULE} bexec ${BUILD_CMD} test_binaries
   HEADERCHECK="headercheck"
 else
