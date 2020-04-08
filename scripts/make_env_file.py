5de6a9174786e2b1b6399d62bce1bd5d0a417923
René Fritze <rene.fritze@wwu.de>
2020-04-08 12:59:57 +0200

    [scripts] try quoting values in make_env

--- scripts/make_env_file.py
+++ scripts/make_env_file.py
@@ -18,10 +18,10 @@ from shlex import quote
 home = expanduser("~")
 
 prefixes = os.environ.get('ENV_PREFIXES', 'BUILD SYSTEM GITLAB CODECOV CI encrypt TOKEN TESTS').split(' ')
-blacklist = ['TRAVIS_COMMIT_MESSAGE', 'CI_COMMIT_MESSAGE']
+blacklist = ['TRAVIS_COMMIT_MESSAGE', 'CI_COMMIT_MESSAGE', 'CI_COMMIT_DESCRIPTION', ]
 env_file = os.environ.get('DOCKER_ENVFILE', os.path.join(home, 'env'))
 with open(env_file, 'wt') as env:
     for k, v in os.environ.items():
         for pref in prefixes:
             if k.startswith(pref) and k not in blacklist:
-                env.write('{}={}\n'.format(k, quote(v)))
+                env.write('{}="{}"\n'.format(k, quote(v)))
