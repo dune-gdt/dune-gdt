fd54406c6d6f21b9375e77725c50eff12f632286
Tobias Leibner <tobias.leibner@googlemail.com>
2021-09-24 16:46:00 +0200

    Apply python formatter

--- docker/update_test_dockers.py
+++ docker/update_test_dockers.py
@@ -41,18 +41,18 @@ except ImportError:
 from docker.utils.json_stream import json_stream
 
 TAG_MATRIX = {
-     'debian-unstable_gcc_full': {
-         'cc': 'gcc',
-         'cxx': 'g++',
-         'deletes': "",
-         'base': 'debian-unstable'
-     },
-     'debian_gcc_full': {
-         'cc': 'gcc',
-         'cxx': 'g++',
-         'deletes': "",
-         'base': 'debian'
-     },
+    'debian-unstable_gcc_full': {
+        'cc': 'gcc',
+        'cxx': 'g++',
+        'deletes': "",
+        'base': 'debian-unstable'
+    },
+    'debian_gcc_full': {
+        'cc': 'gcc',
+        'cxx': 'g++',
+        'deletes': "",
+        'base': 'debian'
+    },
     'debian_clang_full': {
         'cc': 'clang',
         'cxx': 'clang++',
@@ -163,7 +163,13 @@ def _build_combination(tag_matrix, dockerdir, module, commit, refname):
         repo = f'{PROJECT}/{module}-testing_{tag}'
 
         with Timer('docker build ', logger.info):
-            buildargs = {'COMMIT': commit, 'CC': cc, 'project_name': module, 'BASE': settings['base'], 'PROJECT': PROJECT}
+            buildargs = {
+                'COMMIT': commit,
+                'CC': cc,
+                'project_name': module,
+                'BASE': settings['base'],
+                'PROJECT': PROJECT
+            }
             img = _docker_build(
                 client,
                 logger,
