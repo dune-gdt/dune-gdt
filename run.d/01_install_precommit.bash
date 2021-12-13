6d6fcbd8272d8c2e9bae70afe0f3841e6f9d1c80
Tobias Leibner <tobias.leibner@googlemail.com>
2021-12-13 11:22:33 +0100

    [run.d] Install pre-commit during dunecontrol

--- run.d/01_install_precommit.bash
+++ run.d/01_install_precommit.bash
@@ -2,10 +2,5 @@
 
 set -e
 
-cat <<EOF
-****************************************************
-*                                                  *
-*  PLEASE REMEMBER TO `pre-commit install`         *
-*                                                  *
-****************************************************
-EOF
+THIS_DIR="$(cd "$(dirname ${BASH_SOURCE[0]:-$0})" ; pwd -P )"
+cd ${THIS_DIR}/../.. && pre-commit install
