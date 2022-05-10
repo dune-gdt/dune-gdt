2d8fc0fca1a1d95d065fd0d38e2212a926adec4e
René Fritze <rene.fritze@wwu.de>
2022-05-10 11:58:32 +0200

    [ci] cleanout unused files

--- .ci/gitlab/config_template.py
+++ .ci/gitlab/config_template.py
@@ -6,7 +6,7 @@ import jinja2
 from itertools import product
 
 THIS_DIR = Path(__file__).resolve().absolute().parent
-template_filename = THIS_DIR.parent / 'shared/xt_and_gdt_config_template.txt'
+template_filename = THIS_DIR / 'xt_and_gdt_config_template.txt'
 with open(template_filename, 'r') as f:
     tpl = f.read().replace('DUNE_XT_OR_DUNE_GDT', 'dune-xt')
 tpl = jinja2.Template(tpl)
