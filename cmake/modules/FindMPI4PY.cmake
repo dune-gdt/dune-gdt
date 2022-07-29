9827ae649fe90d8b331de75542812321a7b8aaf5
René Fritze <rene.fritze@wwu.de>
2022-07-29 12:43:46 +0200

    DBG mpi find

--- cmake/modules/FindMPI4PY.cmake
+++ cmake/modules/FindMPI4PY.cmake
@@ -17,14 +17,16 @@
 # 1a660df142e9a
 macro(MYFIND_MPI4PY)
   if(NOT MPI4PY_INCLUDE_DIR)
+    set(_include_cmd
+        "import mpi4py,pathlib; incl=str(mpi4py.get_include()).strip();print(pathlib.Path(incl).resolve().absolute())")
     execute_process(
-      COMMAND ${RUN_IN_ENV_SCRIPT} python -c "import mpi4py; print(mpi4py.get_include())"
+      COMMAND "${RUN_IN_ENV_SCRIPT}" "python" "-c" "${_include_cmd}"
       OUTPUT_VARIABLE MPI4PY_INCLUDE_DIR
       RESULT_VARIABLE MPI4PY_COMMAND_RESULT
       OUTPUT_STRIP_TRAILING_WHITESPACE)
     if(MPI4PY_COMMAND_RESULT)
       execute_process(
-        COMMAND "${PYTHON_EXECUTABLE}" "-c" "import mpi4py; print(mpi4py.get_include())"
+        COMMAND "${PYTHON_EXECUTABLE}" "-c" "${_include_cmd}"
         OUTPUT_VARIABLE MPI4PY_INCLUDE_DIR
         RESULT_VARIABLE MPI4PY_COMMAND_RESULT
         OUTPUT_STRIP_TRAILING_WHITESPACE)
@@ -51,6 +53,7 @@ macro(MYFIND_MPI4PY)
 
   include(FindPackageHandleStandardArgs)
   find_package_handle_standard_args(MPI4PY DEFAULT_MSG MPI4PY_INCLUDE_DIR)
+  message(WARNING "include dir for MPI4PY ${MPI4PY_INCLUDE_DIR} via ${MPI4PY_COMMAND_RESULT}")
 endmacro()
 
 myfind_mpi4py()
