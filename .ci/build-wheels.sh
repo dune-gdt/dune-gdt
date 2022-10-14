24e1726e134a731e5a7ebc65d9ddaa69e26e8268
René Fritze <rene.fritze@wwu.de>
2022-10-14 13:53:28 +0200

    [ci] adjust wheelbuilder for split packages

--- .ci/build-wheels.sh
+++ .ci/build-wheels.sh
@@ -22,8 +22,10 @@ ${DUNE_CTRL} --opts=${OPTS} make -j $(nproc --ignore 1) -l $(nproc --ignore 1)
 
 
 ${DUNE_CTRL} --opts=${OPTS} --only=dune-gdt  make -j $(nproc --ignore 1) -l $(nproc --ignore 1) bindings
-python3 -m pip wheel ${DUNE_BUILD_DIR}/dune-gdt/python -w ${WHEEL_DIR}/tmp
+python3 -m pip wheel ${DUNE_BUILD_DIR}/dune-gdt/python/xt/ -w ${WHEEL_DIR}/tmp
+python3 -m pip wheel ${DUNE_BUILD_DIR}/dune-gdt/python/gdt/ -w ${WHEEL_DIR}/tmp
 # Bundle external shared libraries into the wheels
+python3 -m auditwheel repair --plat ${PLATFORM} ${WHEEL_DIR}/tmp/*xt*.whl -w ${WHEEL_DIR}/final
 python3 -m auditwheel repair --plat ${PLATFORM} ${WHEEL_DIR}/tmp/*gdt*.whl -w ${WHEEL_DIR}/final
 
 deactivate
