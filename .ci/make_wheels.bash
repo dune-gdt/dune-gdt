4e86581707b231e17aef2e426368e59def3f3c73
René Fritze <rene.fritze@wwu.de>
2022-12-20 13:18:51 +0100

    [ci] remove no longer needed build script options

--- .ci/make_wheels.bash
+++ .ci/make_wheels.bash
@@ -6,20 +6,16 @@ set -exo pipefail
 git config --global --add safe.directory '*'
 
 THISDIR="$(cd "$(dirname ${BASH_SOURCE[0]})" && pwd -P )"
-md=gdt
 
 [[ -f /usr/local/bin/pybin.sh ]] && source /usr/local/bin/pybin.sh
 
 python3 -m pip install -q twine
-PIP_CONFIG_FILE=${PIP_CONFIG_FILE:-${HOME}/.config/pip/pip.conf}
-[[ -d $(dirname ${PIP_CONFIG_FILE})  ]]  || mkdir -p $(dirname ${PIP_CONFIG_FILE})
-sed "s;PYPI_INDEX_URL;${GITLAB_PYPI}/simple;g" ${THISDIR}/pip.conf > ${PIP_CONFIG_FILE}
 
-${THISDIR}/build-wheels.sh ${md}
+${THISDIR}/build-wheels.sh
 
 echo '************************************'
 echo Wheels are in ${WHEEL_DIR}/final
-python3 -m twine check ${WHEEL_DIR}/final/*${md}*.whl
+python3 -m twine check ${WHEEL_DIR}/final/*.whl
 echo '************************************'
 
 du -sch ${DUNE_BUILD_DIR}/*
