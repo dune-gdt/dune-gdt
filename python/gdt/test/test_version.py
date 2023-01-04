7401c94a6bb7c177f6cc821a88ca94638d3f1a13
René Fritze <rene.fritze@wwu.de>
2023-01-04 08:58:20 +0100

    [ci] setup minimal test for wheel versions

---
+++ python/gdt/test/test_version.py
@@ -0,0 +1,12 @@
+from importlib import metadata
+
+import pytest
+from packaging.version import Version
+
+@pytest.mark.parametrize('module', ['dune.gdt', 'dune.xt'])
+def test_version(module):
+
+    version_str = metadata.version(module)
+    assert version_str is not None
+    assert version_str != 'unknown'
+    assert Version(version_str) > Version('2022.0.0')
