ccd12ce50fc33b5413d94436c318523b9909bf5d
Felix Schindler <ftschindler@noreply.github.com>
2023-02-09 23:04:55 +0100

    update setup scrips

--- build_interactive_bindings_venv.sh
+++ build_interactive_bindings_venv.sh
@@ -6,6 +6,10 @@ if [ "X${OPTS}" == "X" ]; then
     echo No OPTS specified, defaulting to clang-relwithdebinfo.ninja!
     export OPTS=clang-relwithdebinfo.ninja
 fi
+if [ "X${PYTHON}" == "X" ]; then
+    echo No PYTHON specified, defaulting to python3!
+    export PYTHON=python3
+fi
 
 # define environment in case we are not in one of our dockers
 # determines which one to use below environments/
@@ -16,12 +20,12 @@ fi
 
 # initialize the virtualenv, if not yet present
 export BASEDIR="${PWD}"
-mkdir -p environments/${DXT_ENVIRONMENT}/venv
-if [ -e environments/${DXT_ENVIRONMENT}/venv/${OPTS} ]; then
-  echo not creating virtualenv in environments/${DXT_ENVIRONMENT}/venv/${OPTS}, already exists
+mkdir -p deps/environments/${DXT_ENVIRONMENT}/venv
+if [ -e deps/environments/${DXT_ENVIRONMENT}/venv/${OPTS} ]; then
+  echo not creating virtualenv in deps/environments/${DXT_ENVIRONMENT}/venv/${OPTS}, already exists
 else
-  cd environments/${DXT_ENVIRONMENT}/venv
-  virtualenv --python=python3 ${OPTS}
+  cd deps/environments/${DXT_ENVIRONMENT}/venv
+  virtualenv --python=${PYTHON} ${OPTS}
   source ${OPTS}/bin/activate
   deactivate
 fi
@@ -29,7 +33,7 @@ cd "${BASEDIR}"
 unset BASEDIR
 
 # load the variables of this environment, sources the virtualenv
-VENV=environments/${DXT_ENVIRONMENT}/venv/${OPTS} source environments/${DXT_ENVIRONMENT}/PATH.sh
+VENV=deps/environments/${DXT_ENVIRONMENT}/venv/${OPTS} source deps/environments/${DXT_ENVIRONMENT}/PATH.sh
 
 # build dune
 if [ "${OPTS: -6}" == ".ninja" ]; then
@@ -40,20 +44,16 @@ fi
 
 cd "${BASEDIR}"
 # configure
-WERROR=0 ./dune-common/bin/dunecontrol --opts=config.opts/$OPTS --builddir=${BASEDIR}/build-$OPTS configure
-for mod in dune-xt dune-gdt; do
-  WERROR=1 ./dune-common/bin/dunecontrol --opts=config.opts/$OPTS --only=$mod --builddir=${BASEDIR}/build-$OPTS configure
-done
+WERROR=0 ./deps/dune-common/bin/dunecontrol --opts=deps/config.opts/$OPTS --builddir=${BASEDIR}/build-$OPTS configure
+WERROR=1 ./deps/dune-common/bin/dunecontrol --opts=deps/config.opts/$OPTS --only=dune-gdt --builddir=${BASEDIR}/build-$OPTS configure
 # make all
-./dune-common/bin/dunecontrol --builddir=${BASEDIR}/build-$OPTS bexec "$MAKE all"
+./deps/dune-common/bin/dunecontrol --builddir=${BASEDIR}/build-$OPTS bexec "$MAKE all"
 # make bindings
-for mod in dune-xt dune-gdt; do
-  ./dune-common/bin/dunecontrol --builddir=${BASEDIR}/build-$OPTS --only=$mod bexec "$MAKE bindings_no_ext"
-  ./dune-common/bin/dunecontrol --builddir=${BASEDIR}/build-$OPTS --only=$mod bexec "$MAKE install_python"
-done
+./deps/dune-common/bin/dunecontrol --builddir=${BASEDIR}/build-$OPTS --only=dune-gdt bexec "$MAKE bindings_no_ext"
+./deps/dune-common/bin/dunecontrol --builddir=${BASEDIR}/build-$OPTS --only=dune-gdt bexec "$MAKE install_python"
 
 cd "${BASEDIR}"
 echo
 echo "All done! From now on run"
-echo "  OPTS=$OPTS source envs/${DXT_ENVIRONMENT}/PATH.sh"
+echo "  OPTS=$OPTS source deps/environments/${DXT_ENVIRONMENT}/PATH.sh"
 echo "to activate the virtualenv!"
