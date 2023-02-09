1c5dc1d4eb220910a2ac00d45d4a41ea7425b8a9
Felix Schindler <ftschindler@noreply.github.com>
2023-02-09 23:06:49 +0100

    update PATH.sh for docker env

--- deps/environments/debian-full/PATH.sh
+++ deps/environments/debian-full/PATH.sh
@@ -1,7 +1,7 @@
-BASEDIR="$(cd "$(dirname ${BASH_SOURCE[0]})/../.." ;  pwd -P )"
+BASEDIR="$(cd "$(dirname ${BASH_SOURCE[0]})/../../.." ;  pwd -P )"
 export DXT_ENVIRONMENT=${DXT_ENVIRONMENT:-debian-full}
 export OPTS=${OPTS:-clang-relwithdebinfo.ninja}
-export VENV=${VENV:-environments/${DXT_ENVIRONMENT}/venv/${OPTS}}
+export VENV=${VENV:-deps/environments/${DXT_ENVIRONMENT}/venv/${OPTS}}
 export PATH=${BASEDIR}/bin:$PATH
 export F77=gfortran
 export CMAKE_FLAGS="-DDS_HEADERCHECK_DISABLE=1 -DCMAKE_DISABLE_FIND_PACKAGE_MPI=FALSE -DMINIMAL_DEBUG_LEVEL=grave -DDUNE_PYTHON_VIRTUALENV_SETUP=1 -DDUNE_PYTHON_VIRTUALENV_PATH=${BASEDIR}/${VENV}"
