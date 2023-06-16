a1e82cafb5573945b3046e09d2f16ebbdf81cc04
Felix Schindler <ftschindler@noreply.github.com>
2023-06-16 12:55:22 +0200

    docker | make building of dev container more robust

--- docker/build_dev.sh
+++ docker/build_dev.sh
@@ -11,12 +11,21 @@ echo "==========================================================================
 
 docker build --no-cache -t ghcr.io/dune-gdt/dune-gdt/dev:${HASH} -f dev/Dockerfile dev/
 
-echo "======================================================================================================="
-echo "... done!"
-echo "======================================================================================================="
-echo "Execute"
-echo "    docker push ghcr.io/dune-gdt/dune-gdt/dev:${HASH}"
-echo "to push the image and"
-echo "    docker tag ghcr.io/dune-gdt/dune-gdt/dev:${HASH} ghcr.io/dune-gdt/dune-gdt/dev:latest"
-echo "    docker push ghcr.io/dune-gdt/dune-gdt/dev:latest"
-echo "to overwrite the latest tag."
+if [[ $? -eq 0 ]] ; then
+
+    echo "======================================================================================================="
+    echo "... done!"
+    echo "======================================================================================================="
+    echo "Execute"
+    echo "    docker push ghcr.io/dune-gdt/dune-gdt/dev:${HASH}"
+    echo "to push the image and"
+    echo "    docker tag ghcr.io/dune-gdt/dune-gdt/dev:${HASH} ghcr.io/dune-gdt/dune-gdt/dev:latest"
+    echo "    docker push ghcr.io/dune-gdt/dune-gdt/dev:latest"
+    echo "to overwrite the latest tag."
+
+else
+    >&2 echo "======================================================================================================="
+    >&2 echo "... failed (see above)!"
+    >&2 echo "======================================================================================================="
+    exit 1
+fi
