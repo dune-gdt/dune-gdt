a371b6b2941e63738391efc9c056848ed563790f
Felix Schindler <ftschindler@noreply.github.com>
2023-06-16 14:00:32 +0200

    docker | update CI image after dev build

--- docker/build_dev.sh
+++ docker/build_dev.sh
@@ -9,23 +9,38 @@ echo "==========================================================================
 echo "Building ghcr.io/dune-gdt/dune-gdt/dev:${HASH}"
 echo "======================================================================================================="
 
-docker build --no-cache -t ghcr.io/dune-gdt/dune-gdt/dev:${HASH} -f dev/Dockerfile dev/
+docker build -t ghcr.io/dune-gdt/dune-gdt/dev:${HASH} -f dev/Dockerfile dev/
 
-if [[ $? -eq 0 ]] ; then
-
-    echo "======================================================================================================="
-    echo "... done!"
-    echo "======================================================================================================="
-    echo "Execute"
-    echo "    docker push ghcr.io/dune-gdt/dune-gdt/dev:${HASH}"
-    echo "to push the image and"
-    echo "    docker tag ghcr.io/dune-gdt/dune-gdt/dev:${HASH} ghcr.io/dune-gdt/dune-gdt/dev:latest"
-    echo "    docker push ghcr.io/dune-gdt/dune-gdt/dev:latest"
-    echo "to overwrite the latest tag."
-
-else
+if [[ $? != 0 ]] ; then
     >&2 echo "======================================================================================================="
     >&2 echo "... failed (see above)!"
     >&2 echo "======================================================================================================="
     exit 1
 fi
+
+echo "======================================================================================================="
+echo "... done!"
+echo "======================================================================================================="
+echo "Execute"
+echo "    docker push ghcr.io/dune-gdt/dune-gdt/dev:${HASH}"
+echo "to push the image and"
+echo "    docker tag ghcr.io/dune-gdt/dune-gdt/dev:${HASH} ghcr.io/dune-gdt/dune-gdt/dev:latest"
+echo "    docker push ghcr.io/dune-gdt/dune-gdt/dev:latest"
+echo "to overwrite the latest tag."
+echo "======================================================================================================="
+echo "Updating docker/ci/Dockerfile ..."
+
+sed -i \
+    "s;^FROM ghcr.io/dune-gdt/dune-gdt/dev:.*$;FROM ghcr.io/dune-gdt/dune-gdt/dev:${HASH};" \
+    "${BASEDIR}/ci/Dockerfile"
+
+if [[ $? != 0 ]] ; then
+    >&2 echo "======================================================================================================="
+    >&2 echo "... failed (see above)!"
+    >&2 echo "======================================================================================================="
+    exit 1
+fi
+
+echo "... done!"
+echo "Consider to run ./docker/build_ci.sh next!"
+echo "======================================================================================================="
