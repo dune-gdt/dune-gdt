b525ae91d905750e7ab68d31c2fe2382395e0be1
Felix Schindler <ftschindler@noreply.github.com>
2023-06-20 16:01:57 +0200

    docker | ensure hash generation scripts enter the hash

--- docker/compute_ci_hash.sh
+++ docker/compute_ci_hash.sh
@@ -4,7 +4,7 @@ BASEDIR="$(cd "$(dirname ${BASH_SOURCE[0]})" ;  pwd -P)"
 cd "${BASEDIR}"
 
 export DEV_HASH=$(./compute_dev_hash.sh)
-export CI_HASH=$(sha256sum ci/* | sha256sum | cut -d ' ' -f 1)
+export CI_HASH=$(sha256sum ci/* compute_ci_hash.sh | sha256sum | cut -d ' ' -f 1)
 export SUBMODULES_HASH=$(git submodule status | sha256sum | sha256sum | cut -d ' ' -f 1)
 export OPTS_HASH=$(sha256sum ../deps/config.opts/* | sha256sum | cut -d ' ' -f 1)
 echo $(echo $DEV_HASH $CI_HASH $SUBMODULES_HASH $OPTS_HASH | sha256sum | sha256sum | cut -d ' ' -f 1)
