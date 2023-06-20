10318dda5267d640d7bc61c7dc1ba74b0430b596
Felix Schindler <ftschindler@noreply.github.com>
2023-06-20 13:10:06 +0200

    docker | make building of ci image more robust

---
+++ docker/compute_ci_hash.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+BASEDIR="$(cd "$(dirname ${BASH_SOURCE[0]})" ;  pwd -P)"
+cd "${BASEDIR}"
+
+export DEV_HASH=$(./compute_dev_hash.sh)
+export CI_HASH=$(sha256sum ci/* | sha256sum | cut -d ' ' -f 1)
+export SUBMODULES_HASH=$(git submodule status | sha256sum | sha256sum | cut -d ' ' -f 1)
+export OPTS_HASH=$(sha256sum ../deps/config.opts/* | sha256sum | cut -d ' ' -f 1)
+echo $(echo $DEV_HASH $CI_HASH $SUBMODULES_HASH $OPTS_HASH | sha256sum | sha256sum | cut -d ' ' -f 1)
