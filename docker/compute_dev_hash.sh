b679129454af2290722dc881b5a9ab9b31fb8ead
Felix Schindler <ftschindler@noreply.github.com>
2023-06-21 11:42:14 +0200

    ci | make hash computation deterministic by sorting

--- docker/compute_dev_hash.sh
+++ docker/compute_dev_hash.sh
@@ -3,4 +3,4 @@
 BASEDIR="$(cd "$(dirname ${BASH_SOURCE[0]})" ;  pwd -P)"
 cd "${BASEDIR}"
 
-echo $(sha256sum dev/* compute_dev_hash.sh | sha256sum | cut -d ' ' -f 1)
+echo $(sha256sum dev/* compute_dev_hash.sh | sort | sha256sum | cut -d ' ' -f 1)
