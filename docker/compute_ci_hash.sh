cd18e423b69725c6186d00c597b7910f80d48507
Felix Schindler <ftschindler@noreply.github.com>
2023-06-22 23:38:58 +0200

    docker | make ci hash computation more robust w.r.t. missing submodules

--- docker/compute_ci_hash.sh
+++ docker/compute_ci_hash.sh
@@ -5,6 +5,6 @@ cd "${BASEDIR}"
 
 export DEV_HASH=$(./compute_dev_hash.sh)
 export CI_HASH=$(sha256sum ci/* compute_ci_hash.sh | sort | sha256sum | cut -d ' ' -f 1)
-export SUBMODULES_HASH=$(git submodule status | sort | sha256sum | sha256sum | cut -d ' ' -f 1)
+export SUBMODULES_HASH=$(git ls-tree HEAD deps/ | grep commit | sha256sum | sort | sha256sum | cut -d ' ' -f 1)
 export OPTS_HASH=$(sha256sum ../deps/config.opts/* | sort | sha256sum | cut -d ' ' -f 1)
 echo $(echo $DEV_HASH $CI_HASH $SUBMODULES_HASH $OPTS_HASH | sort | sha256sum | cut -d ' ' -f 1)
