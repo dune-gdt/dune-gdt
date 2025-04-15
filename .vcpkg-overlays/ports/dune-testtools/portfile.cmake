764c6f5668fc84481ce217bfd0c0531088d57e04
René Fritze <rene.fritze@arup.com>
2025-04-15 14:03:46 +0200

    update testtools

--- .vcpkg-overlays/ports/dune-testtools/portfile.cmake
+++ .vcpkg-overlays/ports/dune-testtools/portfile.cmake
@@ -1,7 +1,7 @@
 set(VCPKG_BUILD_TYPE release)
 
 vcpkg_from_git(OUT_SOURCE_PATH SOURCE_PATH URL "${CMAKE_CURRENT_LIST_DIR}/../../../deps/dune-testtools" REF
-               1626ea528617dda6ab58f50d5e11de74d1479329)
+               449c352e1803718db5caf4874d331c437f2dbc92)
 
 vcpkg_cmake_configure(SOURCE_PATH "${SOURCE_PATH}" OPTIONS -DBUILD_TESTING=OFF)
 
